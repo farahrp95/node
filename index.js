ed17f650cf387162a96c158009e0d5e4483f3bf0
farahrp95 <129497709+farahrp95@users.noreply.github.com>
2023-04-11 13:25:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var superhero = require("superheroes");
+var mySuperheroName= superhero.random();
+console.log(mySuperheroName);
+var supervilain = require("supervillains");
+var mySupevilainName= superhero.random();
+console.log(mySupevilainName);
